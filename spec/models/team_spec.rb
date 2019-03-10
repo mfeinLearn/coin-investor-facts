5b5db4e0aee392248e73e1a9f6250025a7689b35
github username <github email address>
2019-03-10 15:21:27 -0400

    add specs to the models and controllers

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../spec_helper'
+
+describe Team do
+  it 'has a name'
+  it 'is invalid without a name'
+
+  describe 'associations' do
+    it 'belongs to a InvestmentEntry' #do
+    #   cart = Cart.create
+    #   item1 = Item.create(name: 'iPhone', price: 250, inventory: 70)
+    #   cart.items << item1
+    #
+    #   expect(item1.cart).to eq(cart)
+    # end
+  end
+end
