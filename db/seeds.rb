44de1f00dc7c9f30e8550d73d30c7cfa2d618c6d
github username <github email address>
2019-03-13 13:22:17 -0400

    add other db essentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,30 @@
 
 # Create 2 users
 
-# howard = User.create(name: "Howard", email: "howard@howard.com", password: "password")
-# sherry = User.create(name: "Sherry", email: "sherry@sherry.com", password: "password")
+ homer = User.create(username: "Howard", email: "homer@simpson.com", password: "password")
 
-# Create some journal entries
-
-# JournalEntry.create(title: "Happy Tuesday", content: "Today is Tuesday and it's great!", user_id: howard.id)
+# Create some investment entries
+ datetime = DateTime.now
+ investment_entry1 = InvestmentEntry.create(:coin_name => "SingularityNET", :community => "https://t.me/singularitynet", :code => "https://github.com/singnet", :whitepaper => "https://public.singularitynet.io/whitepaper.pdf", :user_id => homer.id, :date => datetime)
 
 # Use AR to per-associate data:
 
-# howard.journal_entries.create(title: "My Second Entry", content: "We are building a Sinatra app! Yaay!")
+ homer.investment_entries.create(:coin_name => "Ethereum", :community => "https://gitter.im/ethereum/home", :code => "https://github.com/ethereum", :whitepaper => "https://github.com/ethereum/wiki/wiki/White-Paper", :user_id => homer.id, :date => datetime)
+
+ #sherrys_entry = sherry.journal_entries.build(title: "Cold VA", content: "It's super cold in VA!!")
+ #sherrys_entry.save
+
+# characters:
 #
-# sherrys_entry = sherry.journal_entries.build(title: "Cold VA", content: "It's super cold in VA!!")
-# sherrys_entry.save
+# Stan Marsh
+# Kyle Broflovski
+# Eric Cartman
+# Kenny McCormick
+#
+# Philip J. Fry
+# Turanga Leela
+# Bender
+# Professor Farnsworth
+# Hermes Conrad
+# Doctor Zoidberg
+# Amy Wong
