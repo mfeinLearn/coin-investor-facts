423a2729afbe27370be9da2a46c64ed5e0854139
github username <github email address>
2019-03-13 13:11:06 -0400

    add controller actions to the users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/investment_entries"
+    else
+      erb :"users/create_user"
+    end
+  end
+
+  post '/signup' do
+    #binding.pry
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      # create a new user
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/investment_entries"
+
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/investment_entries'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+      #raise params.inspect
+      session[:user_id] = @user.id # actually logging the user in
+      redirect "/investment_entries"
+    else
+      redirect "/signup"
+    end
+  end
+
+  # user SHOW route
+  # this routes job is to show the user
+  get '/users/:slug' do # this will be the user show route
+    # what do I need to do first?
+    # raise params.inspect
+    @user = User.find_by_slug(params[:slug])
+    #  redirect_if_not_logged_in
+    erb :'users/show'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
+
+end
