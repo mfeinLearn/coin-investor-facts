39999a8e4e70c808b218a6812f6902b31b298a01
github username <github email address>
2019-03-15 11:45:27 -0400

    add validations to User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :username, :email, :password, presence: true
+  validates :email, uniqueness: true
+  
   has_secure_password
   has_many :investment_entries
 
